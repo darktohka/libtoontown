d6e6480e6ed717c51d4e99ee287719b29ad3709a
Drew C <31282527+drewc5131@users.noreply.github.com>
2020-09-17 11:10:54 -0400

    [Sign] Enable depth writing to fix text flickering

--- src/toontown/dna/dnaSign.cxx
+++ src/toontown/dna/dnaSign.cxx
@@ -78,9 +78,7 @@ NodePath DNASign::traverse(NodePath &parent, DNAStorage *store, int editing) {
     sign_node_path = building_front.attach_new_node(new ModelNode("sign"));
   }
   nassertr(!sign_node_path.is_empty(), parent);
-
-  // Turn off the writing of z-buffer information:
-  sign_node_path.set_depth_write(0);
+  
   //sign_node_path.node()->set_name("sign");
 
   // The Sign_origin is a special node in the model with a local
